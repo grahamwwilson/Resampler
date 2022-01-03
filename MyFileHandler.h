a3f32ae6122927694a834617eb3035f70e76ded6
grahamwwilson <gwwilson@ku.edu>
2022-01-02 18:54:46 -0600

    Better integrated file handling. Add bin edge files

---
+++ MyFileHandler.h
@@ -0,0 +1,72 @@
+// MyFileHandler.h
+
+std::vector<std::pair<double,double>> CreateX1X2PairVector(std::string filename);
+std::vector<double> CreateECMVector(std::vector<std::pair<double,double>>& v);
+std::vector<double> CreateEDiffVector(std::vector<std::pair<double,double>>& v);
+std::vector<double> CreateBinVector(std::string filename);
+
+std::vector<std::pair<double,double>> CreateX1X2PairVector(std::string filename){
+
+// Open input data set
+   std::ifstream inputDataFile(filename);
+
+   std::vector<std::pair<double,double>> v;
+
+   if (inputDataFile) {
+       int nheader;
+       inputDataFile >> nheader;
+       double x1,x2;
+       while ( inputDataFile >> x1 >> x2 ) {
+           v.push_back(std::make_pair(x1,x2));
+       }
+   }
+   inputDataFile.close();  
+   return v;
+}
+
+std::vector<double> CreateBinVector(std::string filename){
+
+// Open input file with bin values
+   std::ifstream inputDataFile(filename);
+
+   std::vector<double> v;
+
+   if (inputDataFile) {
+       int ibin;
+       double xbin;
+       while ( inputDataFile >> ibin >> xbin ) {
+           v.push_back(xbin);
+       }
+   }
+   inputDataFile.close();  
+   return v;   
+
+}
+
+std::vector<double> CreateECMVector(std::vector<std::pair<double,double>>& v){
+
+   std::vector<double> vout;
+   std::vector<std::pair<double,double>>::const_iterator it;
+   
+   for (it = v.begin(); it!=v.end(); ++it){
+       std::pair<double,double> thispair = *it;
+       double x1 = thispair.first;
+       double x2 = thispair.second;
+       vout.push_back(sqrt(x1*x2));
+   }
+   return vout;
+}
+
+std::vector<double> CreateEdiffVector(std::vector<std::pair<double,double>>& v){
+
+   std::vector<double> vout;
+   std::vector<std::pair<double,double>>::const_iterator it;
+   
+   for (it = v.begin(); it!=v.end(); ++it){
+       std::pair<double,double> thispair = *it;
+       double x1 = thispair.first;
+       double x2 = thispair.second;
+       vout.push_back(x1-x2);
+   }
+   return vout;    
+}
---
+++ run16.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+#
+# Here we no longer necessarily make the data/MC distinction
+#
+date
+# Might as well recompile it here too
+#g++ -g -o example8 example8.cpp `root-config --cflags --glibs`
+
+export OMP_NUM_THREADS=6
+
+echo 'OMP_NUM_THREADS: '$OMP_NUM_THREADS
+
+g++ -g -O1 -fopenmp -o example example11.cpp `root-config --cflags --glibs`
+
+# Set up symbolic links used in code.
+VDATA=${1:-1}
+VMC=${2:-1}
+
+echo 'VDATA '${VDATA}
+echo 'VMC   '${VMC}
+
+rm dataset1.cdat
+rm dataset2.cdat
+
+ln -s datasubset${VDATA}.cdat dataset1.cdat
+ln -s mcsubset${VMC}.cdat dataset2.cdat
+
+# Check symbolic links
+ls -l *.cdat
+
+# Can be quicker for some estimators if the smaller file 
+# is set as the first "DATA" set.
+
+NPERMS=${3:-100}
+NDATA=${4:-1000}
+NMC=${5:-1000}
+FIRSTSEED=${6:-123456}
+# Now specify statistic choice using Estimator enum with corresponding EstimatorNames string
+ESTIMATOR=${7:-Energy2}
+
+echo 'Parameters set to (NDATA,NMC,NPERMS,FIRSTSEED,ESTIMATOR) '${NDATA} ${NMC} ${NPERMS} ${FIRSTSEED} ${ESTIMATOR}
+time ./example ${NDATA} ${NMC} ${NPERMS} ${FIRSTSEED} ${ESTIMATOR}
+
+date
+
+exit
